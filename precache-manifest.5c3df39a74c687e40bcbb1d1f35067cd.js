378b4a98cc1aac9d579a2c10615ffab1d4efcabc
ozenesat <ozenesat@gmail.com>
2020-05-08 19:18:14 -0400

    Updates

---
+++ precache-manifest.5c3df39a74c687e40bcbb1d1f35067cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5658060583ef2eb6082b81e7ce2d493e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a052c91dec412c18145a",
+    "url": "/my-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a052c91dec412c18145a",
+    "url": "/my-app/static/js/2.08f0c78a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.08f0c78a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526eaed1483587115228",
+    "url": "/my-app/static/js/main.a22ff61e.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  }
+]);
